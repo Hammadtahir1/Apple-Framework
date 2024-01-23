ffee7006d59bc309f0a1ab96da57b55de4a73592
Hammad Tahir <hammadtahir2004@gmail.com>
2024-01-23 12:53:59 +0500

    project added

---
+++ Apple FrameWork/Apple FrameWork/Apple_FrameWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorkApp.swift
+//  Apple FrameWork
+//
+//  Created by Hamza on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
